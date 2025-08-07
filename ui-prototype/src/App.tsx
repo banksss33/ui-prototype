63de55ddd496d427457b0741cbc6c66b9c00ed7f
CarToonCH <cartoon_happy46@hotmail.com>
2025-08-07 23:59:37 +0700

    update package , main css and tsx
    - add tailwind ,postcss in package
    - ad tailwind config
    - update vite
    - update app page and css

--- ui-prototype/src/App.tsx
+++ ui-prototype/src/App.tsx
@@ -8,26 +8,40 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      {/* <button>HEE + {count}</button> */}
+
+      <div className="flex flex-col items-center justify-center w-screen h-screen gap-6 bg-amber-400">
+        {/* <div
+					className="button-square"
+				>
+					<span className="flex flex-col justify-center items-center h-full text-white font-bold text-lg ">
+						Active
+					</span>
+
+				</div> */}
+        
+        {/* <div>
+          <Button>test</Button>
+        </div> */}
+
+        <button
+          className="button-square"
+          onClick={() => setCount((prev) => prev + 1)}
+        >
+          Count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+        <div className={`button-circle mid`}>
+          <span className="flex flex-col justify-center items-center h-full text-white font-bold text-lg ">
+            Love Me
+          </span>
+        </div>
+        <div className="button-circle circle">
+          <span className="flex flex-col justify-center items-center h-full text-white font-bold text-lg ">
+            ?
+          </span>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
